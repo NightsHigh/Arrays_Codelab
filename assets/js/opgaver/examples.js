032473dff468e2f77386d3121a811f88abbf4d55
eske <eske.larsen@gmail.com>
2025-08-25 12:49:58 +0200

    Completed opgave 3 / red

--- assets/js/opgaver/examples.js
+++ assets/js/opgaver/examples.js
@@ -7,21 +7,29 @@ myExtrasInput.addEventListener("change", addExtra);
 
 
 
-function addExtra() {
+function addExtra(textString) {
     /* din map kode her... */
+    myExtras.push(extraUdstyr.value);
+    updateExtras();
+
 }
 
 
 
 function updateExtras() {
-
-
-
+    // Clear the list before adding new items
+    myExtrasListElement.innerHTML = '';
 
     /* din map kode her... */
-
-
+    myExtras.forEach(i => {
+        const li = document.createElement('li');
+        li.textContent = i;
+        myExtrasListElement.appendChild(li);
+    });
 }
+
+
+
 
 
 
