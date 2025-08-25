f80b3c817f30cacf0ada59fbcffb7d49ecf90570
eske <eske.larsen@gmail.com>
2025-08-25 13:08:25 +0200

    Completed red opgave

--- assets/js/opgaver/examples.js
+++ assets/js/opgaver/examples.js
@@ -1,17 +1,16 @@
 let myExtras = [];
 const myExtrasInput = document.getElementById('extraUdstyr');
 const myExtrasListElement = document.getElementById('extraList');
-
-
+const mySelectedElements = document.getElementById('selectedElements');
+mySelectedElements.addEventListener("click", removeSelected);
 myExtrasInput.addEventListener("change", addExtra);
 
 
 
-function addExtra(textString) {
+function addExtra() {
     /* din map kode her... */
     myExtras.push(extraUdstyr.value);
     updateExtras();
-
 }
 
 
@@ -19,20 +18,25 @@ function addExtra(textString) {
 function updateExtras() {
     // Clear the list before adding new items
     myExtrasListElement.innerHTML = '';
-
+    mySelectedElements.innerHTML = '';
     /* din map kode her... */
     myExtras.forEach(i => {
         const li = document.createElement('li');
         li.textContent = i;
         myExtrasListElement.appendChild(li);
+
+
+        const newlySelectedOption = document.createElement('option');
+        newlySelectedOption.value = i;
+        newlySelectedOption.textContent = i;
+        mySelectedElements.appendChild(newlySelectedOption);
     });
 }
 
 
-
-
-
-
-
-
+function removeSelected(){
+    const selectedOption = mySelectedElements.value;
+    myExtras = myExtras.filter(i => i !== selectedOption);
+    updateExtras();
+}
 
